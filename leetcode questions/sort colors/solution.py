f4b186fe5d77203bd67e872e53ccf9d9945872cf
Tornike Buchukuri <tornike.buchukuri.314@gmail.com>
2022-01-23 15:15:01 +0400

    sort colors

---
+++ leetcode questions/sort colors/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortColors(self, nums):
+        swapped = True
+        while swapped:
+            swapped = False
+            for i in range(len(nums) - 1):
+                if nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                    swapped = True
